1634a9a45beed6342eb20544f5ddc9a9b43a9d10
repkit <ionut.baches@yahoo.com>
2016-06-14 22:37:05 +0000

    added new db structure

---
+++ data/rpkcms-struct[20160614].sql
@@ -0,0 +1,445 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2016 at 10:27 PM
+-- Server version: 5.5.46-0ubuntu0.14.04.2
+-- PHP Version: 5.5.9-1ubuntu4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `rpkcms`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+DROP PROCEDURE IF EXISTS `fetchAllPages`$$
+CREATE DEFINER=`repkit`@`%` PROCEDURE `fetchAllPages`(IN filters TEXT, IN sorters TEXT, IN startfrom INT(4), IN endto INT(4))
+BEGIN
+SET @sql = NULL;
+SELECT
+  GROUP_CONCAT(DISTINCT
+    CONCAT(
+      'MAX(IF(page_meta.metaKey = ''',
+      page_meta.metaKey,
+      ''', page_meta.metaValue, NULL)) AS ',
+      page_meta.metaKey
+    )
+  ) INTO @sql
+FROM page_meta;
+
+SET @sql = CONCAT(' SELECT pages.*, pagePathByCategoryId(pages.categoryid) AS path, ', 
+                    @sql, 
+                    ' FROM pages
+                    LEFT JOIN page_meta
+                    ON pages.id = page_meta.pageId ',
+                    filters,
+                    ' GROUP BY pages.id ORDER BY ', sorters,
+                    ' LIMIT ',startfrom,' , ', endto);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+END$$
+
+--
+-- Functions
+--
+DROP FUNCTION IF EXISTS `categoryChildrenById`$$
+CREATE DEFINER=`repkit`@`%` FUNCTION `categoryChildrenById`(`categoryid` INT) RETURNS text CHARSET utf8
+    DETERMINISTIC
+BEGIN
+
+    DECLARE rv,q,queue,queue_children VARCHAR(1024);
+    DECLARE queue_length,front_id,pos INT;
+
+    SET rv = '';
+    SET queue = categoryid;
+    SET queue_length = 1;
+
+    WHILE queue_length > 0 DO
+        SET front_id = FORMAT(queue,0);
+        IF queue_length = 1 THEN
+            SET queue = '';
+        ELSE
+            SET pos = LOCATE(',',queue) + 1;
+            SET q = SUBSTR(queue,pos);
+            SET queue = q;
+        END IF;
+        SET queue_length = queue_length - 1;
+
+        SELECT IFNULL(qc,'') INTO queue_children
+        FROM (SELECT GROUP_CONCAT(id) qc
+        FROM page_categories WHERE parentId = front_id) A;
+
+        IF LENGTH(queue_children) = 0 THEN
+            IF LENGTH(queue) = 0 THEN
+                SET queue_length = 0;
+            END IF;
+        ELSE
+            IF LENGTH(rv) = 0 THEN
+                SET rv = queue_children;
+            ELSE
+                SET rv = CONCAT(rv,',',queue_children);
+            END IF;
+            IF LENGTH(queue) = 0 THEN
+                SET queue = queue_children;
+            ELSE
+                SET queue = CONCAT(queue,',',queue_children);
+            END IF;
+            SET queue_length = LENGTH(queue) - LENGTH(REPLACE(queue,',','')) + 1;
+        END IF;
+    END WHILE;
+
+    RETURN rv;
+
+END$$
+
+DROP FUNCTION IF EXISTS `pagePathByCategoryId`$$
+CREATE DEFINER=`repkit`@`%` FUNCTION `pagePathByCategoryId`(`categoryid` INT) RETURNS text CHARSET utf8
+    DETERMINISTIC
+BEGIN
+  
+	DECLARE rv TEXT;
+    DECLARE cm CHAR(1);
+    DECLARE ch INT;
+    DECLARE nm VARCHAR(255);
+
+    SET rv = '';
+    SET cm = '';
+    SET ch = categoryid;
+	SET nm = '';
+    WHILE ch > 0 DO
+        SELECT parentId, slug INTO ch, nm FROM
+        (SELECT parentId, slug FROM page_categories WHERE id = ch) A;
+        IF ch >= 0 THEN
+            SET rv = CONCAT(nm,cm,rv);
+            SET cm = '/';
+        END IF;
+    END WHILE;
+    RETURN CONCAT(cm,rv);
+
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE IF NOT EXISTS `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `templateId` int(11) NOT NULL,
+  `parentId` int(11) NOT NULL DEFAULT '0',
+  `categoryId` int(11) NOT NULL DEFAULT '0',
+  `roleId` int(11) NOT NULL DEFAULT '0',
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `statusId` int(11) NOT NULL,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `templateId` (`templateId`),
+  KEY `parentId` (`parentId`),
+  KEY `categoryId` (`categoryId`),
+  KEY `roleId` (`roleId`),
+  KEY `priority` (`priority`),
+  KEY `statusId` (`statusId`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_categories`
+--
+
+DROP TABLE IF EXISTS `page_categories`;
+CREATE TABLE IF NOT EXISTS `page_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentId` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`),
+  KEY `parentId` (`parentId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_categories_nested_model`
+--
+
+DROP TABLE IF EXISTS `page_categories_nested_model`;
+CREATE TABLE IF NOT EXISTS `page_categories_nested_model` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentId` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lft` int(11) DEFAULT NULL,
+  `rgt` int(11) DEFAULT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`),
+  KEY `lft` (`lft`,`rgt`),
+  KEY `parentId` (`parentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_meta`
+--
+
+DROP TABLE IF EXISTS `page_meta`;
+CREATE TABLE IF NOT EXISTS `page_meta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pageId` int(11) NOT NULL,
+  `metaKey` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `metaValue` text COLLATE utf8_unicode_ci NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pageId` (`pageId`,`metaKey`),
+  KEY `metaKey` (`metaKey`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=18 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_meta_tags`
+--
+
+DROP TABLE IF EXISTS `page_meta_tags`;
+CREATE TABLE IF NOT EXISTS `page_meta_tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pageId` int(11) NOT NULL,
+  `value` text COLLATE utf8_unicode_ci NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `pageId` (`pageId`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=16 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_schedules`
+--
+
+DROP TABLE IF EXISTS `page_schedules`;
+CREATE TABLE IF NOT EXISTS `page_schedules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pageId` int(11) NOT NULL,
+  `oldPageStausId` int(11) DEFAULT NULL,
+  `newPageStatusId` int(11) NOT NULL,
+  `scheduleDate` datetime NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `pageId` (`pageId`),
+  KEY `scheduleDate` (`scheduleDate`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_statuses`
+--
+
+DROP TABLE IF EXISTS `page_statuses`;
+CREATE TABLE IF NOT EXISTS `page_statuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_templates`
+--
+
+DROP TABLE IF EXISTS `page_templates`;
+CREATE TABLE IF NOT EXISTS `page_templates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `path` text COLLATE utf8_unicode_ci NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_path` (`name`,`path`(200)),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `value` text COLLATE utf8_unicode_ci NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(4095) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `nickname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `registrationDate` datetime NOT NULL,
+  `activationKey` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `external` int(11) NOT NULL DEFAULT '0',
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`,`password`(255)),
+  KEY `external` (`external`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`),
+  KEY `email` (`email`,`password`(255))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_roles`
+--
+
+DROP TABLE IF EXISTS `users_roles`;
+CREATE TABLE IF NOT EXISTS `users_roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `roleId` int(11) NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  KEY `roleId` (`roleId`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_meta`
+--
+
+DROP TABLE IF EXISTS `user_meta`;
+CREATE TABLE IF NOT EXISTS `user_meta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `metaKey` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `metaValue` text COLLATE utf8_unicode_ci NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`,`metaKey`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=45 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+CREATE TABLE IF NOT EXISTS `user_roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `parentId` int(11) NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `modificationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `authorId` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `parentId` (`parentId`),
+  KEY `authorId` (`authorId`),
+  KEY `state` (`state`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
